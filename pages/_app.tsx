9efc4898b6255b4f7c3c84548e00488dbf7309a2
Laporte Andoni <lapand@hotmail.fr>
2024-05-09 19:29:22 +0200

    add React.StrictMode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import i18n from "@/modules/i18n";
 import { I18nextProvider } from 'react-i18next';
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <I18nextProvider i18n={i18n}>
-      <Component {...pageProps} />
-    </I18nextProvider>
+    <React.StrictMode>
+      <I18nextProvider i18n={i18n}>
+        <Component {...pageProps} />
+      </I18nextProvider>
+    </React.StrictMode>
   );
 }
